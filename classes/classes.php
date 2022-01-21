26db6fd8c4191a20a71d88d473a4b162d59acd72
rubskaiserman <rubskaiserman@gmail.com>
2022-01-21 12:55:44 -0300

    Creation of the object user finished

--- classes/classes.php
+++ classes/classes.php
@@ -3,10 +3,18 @@
     class User{
         public $userId;
         public $name;
-        public $senha;
+        public $email;
+        public $password;
 
-        function createUser(){
+        public function __construct(String $email, String $name, String $password){
+            $this->email = $email;
+            $this->name = $name;
+            $this->password = $password;
+        }
+
+        public function createUser(){
             //cria o user no banco de dados
+            //Linkar o user com o userId
         }
     }
 
--- signup/signup.php
+++ signup/signup.php
@@ -1,7 +1,8 @@
 <?php 
     include "../classes/classes.php";
 
-    $user = new User();
-    $element = 'name';
-    echo $user->$element;
+    $user = new User($_POST["email"], $_POST["name"], $_POST["password"]);
+    header("Location: ../index.php");
+    exit();
+    
 ?>
