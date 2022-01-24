405dc7d72cc5e779d05439b719bca5acbd90b951
rubskaiserman <rubskaiserman@gmail.com>
2022-01-24 15:29:33 -0300

    Adding some stuff

--- js/main.js
+++ js/main.js
@@ -1,4 +1,18 @@
 function showForm(){
     document.querySelector("form").style.display = "block"
     document.querySelector("#title").focus()
+
+    document.querySelectorAll(".time")[0].style.display = "flex"
+    document.querySelectorAll(".time")[1].style.display = "flex"
+
+}
+
+function unableTime(){
+    if(document.querySelectorAll(".time")[0].style.display == "flex"){
+        document.querySelectorAll(".time")[0].style.display = "none"
+        document.querySelectorAll(".time")[1].style.display = "none"
+    } else {
+        document.querySelectorAll(".time")[0].style.display = "flex"
+        document.querySelectorAll(".time")[1].style.display = "flex"
+    }
 }
