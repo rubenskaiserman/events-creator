7895b42af3c574ef04480c5954d5c3cae8e485b0
rubskaiserman <rubskaiserman@gmail.com>
2022-01-26 13:49:11 -0300

    Criação de eventos concluida

--- create-event.php
+++ create-event.php
@@ -1,8 +1,10 @@
 <?php 
 
     ini_set('display_errors', 1);
+    echo $_COOKIE["userId"];
     $resource = pg_connect("host=localhost port=5432 dbname=events_creator user=creator password=events") or die("could not connect");
-    
-    pg_insert($resource, 'events', $_POST);
-    header("Location: ../index.php");
+    $_POST = array_merge($_POST, array("userid"=>$_COOKIE["userId"]));
+    echo $_POST["userid"];
+    pg_insert($resource, 'events', $_POST );
+    // header("Location: ../index.php");    
 ?>
