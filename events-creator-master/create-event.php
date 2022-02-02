78f3e0750c0aa9b0f050383c8a4fbef1a7c53e2f
ZCometa <ZCometa@tfgames.site>
2022-02-02 17:41:53 -0300

    aoba

--- events-creator-master/create-event.php
+++
@@ -1,7 +0,0 @@
-<?php 
-    ini_set('display_errors', 1);
-    $resource = pg_connect("host=localhost port=5432 dbname=events_creator user=creator password=events") or die("could not connect");
-    $_POST = array_merge($_POST, array("userid"=>$_COOKIE["userId"]));
-    pg_insert($resource, 'events', $_POST );
-    header("Location: ../index.php");    
-?>
