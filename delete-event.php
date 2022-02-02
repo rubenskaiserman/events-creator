1d1961bc546d5db21d94c36fa456322f60c0ba3a
rubskaiserman <rubskaiserman@gmail.com>
2022-02-02 19:43:14 -0300

    Eu não tô entendendo porque tá funcionando pra mim

--- delete-event.php
+++ delete-event.php
@@ -1,6 +1,6 @@
 <?php 
     ini_set('display_errors', 1);
     $resource = pg_connect("host=localhost port=5432 dbname=events_creator user=creator password=events") or die("could not connect");
-    pg_query($resource, "DELETE FROM events WHERE eventid={$_POST[event]}");
+    pg_query($resource, "DELETE FROM events WHERE eventid={$_POST['event']}");
     header("Location: index.php");    
 ?>
