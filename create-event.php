e57cddede06d16424bb9a021b7a3496c6634f0f5
rubskaiserman <rubskaiserman@gmail.com>
2022-02-02 19:25:54 -0300

    Testa aí

--- create-event.php
+++ create-event.php
@@ -3,5 +3,5 @@
     $resource = pg_connect("host=localhost port=5432 dbname=events_creator user=creator password=events") or die("could not connect");
     $_POST = array_merge($_POST, array("userid"=>$_COOKIE["userId"]));
     pg_insert($resource, 'events', $_POST );
-    header("Location: ../index.php");    
+    header("Location: index.php");    
 ?>
